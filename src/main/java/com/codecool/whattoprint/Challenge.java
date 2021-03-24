15af2540a55dac8377b872e81a477998a81e7e56
Misi <misi.magenheim@gmail.com>
2021-03-24 10:21:23 +0100

    ReadyToPush

--- src/main/java/com/codecool/whattoprint/Challenge.java
+++ src/main/java/com/codecool/whattoprint/Challenge.java
@@ -2,6 +2,21 @@ package com.codecool.whattoprint;
 
 class Challenge {
     public static String decideAndSay(String[] words, int number) {
-        return words[0];
+        String res = "";
+        for( int i = 0; i < words.length; i++ ) {
+            if (number % 21 == 0 ) {
+                res = words[0] + words[1];
+            }
+            else if (number % 3 == 0 && number % 7 != 0) {
+                res = words[0];
+            }
+            else if (number % 3 != 0 && number % 7 == 0) {
+                res = words[1];
+            }
+            else {
+                res = "";
+            }
+        }
+        return res;
     }
 }
